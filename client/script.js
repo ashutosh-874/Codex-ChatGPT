5aa0042a55d591a746cd3edc4ce2c6e1c625a54c
ashutosh-874 <64722998+ashutosh-874@users.noreply.github.com>
2023-01-02 21:26:10 +0530

    Added live server link

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
 	loader(messageDiv)
 	
 	// fetch data from server
-	const response = await fetch("http://localhost:5000", {
+	const response = await fetch("https://codex-chatgpt-4bd5.onrender.com", {
 		method : "POST",
 		headers: {
 			"Content-Type": "application/json"
